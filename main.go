225b4b1fb96ab4452cad9c9b4a00606d0d178c35
Wang Yaoshen <wangyaoshen@bgi.com>
2019-01-25 17:36:18 +0800

    add port flag

--- main.go
+++ main.go
@@ -1,24 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+var (
+	port = flag.String(
+		"port",
+		":9091",
+		"web server listen port",
+	)
+)
+
 func main() {
-	// Files to Zip
-	files := []string{"example.csv", "data.csv"}
-	output := "done.zip"
-
-	err := ZipFiles(output, files)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Zipped File: " + output)
-	http.HandleFunc("/login", login) //设置访问的路由
+	flag.Parse()
+	// 设置访问的路由
+	http.HandleFunc("/login", login)
 	http.HandleFunc("/upload", upload)
 	http.HandleFunc("/upload2", upload2)
 	http.HandleFunc("/upload2debug", upload2debug)
@@ -36,9 +36,8 @@ func main() {
 		//		}
 
 	}) //设置访问的路由
-	//http.HandleFunc("/", sayhelloName)         //设置访问的路由
 	fmt.Println("start")
-	err = http.ListenAndServe(":9091", nil) //设置监听的端口
+	err := http.ListenAndServe(*port, nil) //设置监听的端口
 	if err != nil {
 		log.Fatal(err)
 	}
